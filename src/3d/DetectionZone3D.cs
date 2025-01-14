efdfb64bbc31db1053d1b796afa150319daabbad
Timit0 <132546200+Timit0@users.noreply.github.com>
2025-01-14 21:24:21 +0100

    Fix plugin error when changing Godot windows (set script) I've changed Area3D to DetectionZone3D where inherit from Area3D

--- src/3d/DetectionZone3D.cs
+++ src/3d/DetectionZone3D.cs
@@ -1,6 +1,8 @@
+#if TOOLS
 using Godot;
 using System;
 
+[Tool]
 public partial class DetectionZone3D : Area3D
 {
     // Called when the node enters the scene tree for the first time.
@@ -11,6 +13,11 @@ public partial class DetectionZone3D : Area3D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
-        GD.Print("GGG");
+        // foreach (Node node in GetOverlappingBodies())
+        // {
+        //     GD.Print(node.Name);
+        // }
+        GD.Print("A");
     }
 }
+#endif
